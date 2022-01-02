f6b4b5f9838238b80373d0f3ad695103e7e8a6d9
SergeiKotov <43482705+SergeiKotov@users.noreply.github.com>
2022-01-02 13:56:48 -0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+bricks = [10, 2, 5, 6]
+print(len(bricks))
+for k in range(len(bricks)):
+    
+    print(k, bricks[k])
+    # del(bricks[k])
+del(bricks[0])
+print(bricks)
+bricks = []
+print(bricks)
